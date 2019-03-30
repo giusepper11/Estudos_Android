8537350d792287d3fada1d057f7d7abaa8c11828
Giuseppe Rosa <giusepper11@gmail.com>
2019-03-30 19:25:06 -0300

    Salvando arquivos de fotos unicas

--- Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -1,6 +1,9 @@
 package br.com.alura.agenda;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -20,7 +24,9 @@ import br.com.alura.agenda.modelo.Aluno;
 
 public class FormularioActivity extends AppCompatActivity {
 
+    public static final int CODIGO_CAMERA = 567;
     private FormularioHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +43,33 @@ public class FormularioActivity extends AppCompatActivity {
         }
 
         Button botaoFoto = (Button) findViewById(R.id.formulario_botao_foto);
-        assert botaoFoto != null;
         botaoFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                String caminhoFoto = "/" + System.currentTimeMillis() + ".jpg";
-                File arquivoFoto = new File(getExternalFilesDir(null) + (caminhoFoto));
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
                 intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
-                startActivity(intentCamera);
+                startActivityForResult(intentCamera, CODIGO_CAMERA);
 
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //abrir a foto
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CODIGO_CAMERA) {
+                ImageView foto = (ImageView) findViewById(R.id.formulario_foto);
+                Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+                Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+                foto.setImageBitmap(bitmapReduzido);
+                foto.setScaleType(ImageView.ScaleType.FIT_XY);
+            }
+        }
+
 
     }
 
