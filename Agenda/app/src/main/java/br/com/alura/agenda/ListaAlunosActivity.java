9af4632a413d91df8214f7d1f5d7c557333db8d7
Giuseppe Rosa <giusepper11@gmail.com>
2019-03-30 17:48:23 -0300

    Permissões, SMS, mapa e navegador

--- Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.alura.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
@@ -84,13 +87,33 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         //Mandar sms
         MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSite.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
 
         //Achar no mapa
         MenuItem itemMap = menu.add("Localizar no mapa");
+        Intent intentMap = new Intent(Intent.ACTION_VIEW);
+        intentMap.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMap.setIntent(intentMap);
 
         //Ligar
-        MenuItem itemCall = menu.add("Ligar");
-
+        MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE}, 123);
+                } else {
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
         //Deletar Aluno
         MenuItem deletar = menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
